915218c080585291a5b6418bda31aad4aedb3252
tony10101105 <b08901133@ntu.edu.tw>
2021-07-04 22:53:03 +0800

    test

--- DataSet.py
+++ DataSet.py
@@ -27,34 +27,39 @@ class DataSet(Dataset):
 
     def __init__(self, mode = 'non-fraud', normalization_type = 'mix_max'):
 
-        if mode == 'non-fraud':
-            CREDIT_CARD_DIRECTORY = './datasets/creditcard_0.csv'
-        elif mode == 'fraud':
-            CREDIT_CARD_DIRECTORY = './datasets/creditcard_1.csv'
+        CREDIT_CARD_DIRECTORY = './datasets/Kaggle_CCFD/creditcard.csv'
 
-        ##preprocessing of numerical data
+        # data loading
         self.features = []
         self.labels = []
         csvCreditCard = open(CREDIT_CARD_DIRECTORY)
         CreditCardData = csv.reader(csvCreditCard)
         
+        if mode == 'non-fraud':
+            skipped_class = '1'
+        elif mode == 'fraud':
+            skipped_class = '0'
+
         for row in CreditCardData:
+            if row[-1] == skipped_class or row[-1] == 'Class':
+                continue
             self.features.append(row[:-1])
             self.labels.append(row[-1])
             
-        #####convert elements from string to float
+        # convert elements from string to float
         for i in range(len(self.features)):
             self.features[i] = list(map(float, self.features[i]))
         self.labels = list(map(float, self.labels))
-        #####
         
+        # normalization
         if normalization_type == 'mix_max':
             self.features = mix_max_normalization(self.features)
         elif normalization_type == 'z_score':
             self.features = z_score_normalization(self.features)
         else:
             raise Exception('this type of normalization not implemented yet')
-
+        
+        # conversion to tensor
         self.features = torch.FloatTensor(self.features)
         self.labels = torch.FloatTensor(self.labels)
         

c9573323b951cd084c020cbe92d0c9fbf80e42df
noidname01 <tim983649@gmail.com>
2021-07-19 14:35:41 +0800

    threshold precision

--- ocnn.py
+++ ocnn.py
@@ -19,7 +19,7 @@ nbrs.fit(train_data[:,:-1])
 
 distances, indexes = nbrs.kneighbors(test_data[:,:-1])
 
-print(distances)
+# print(distances)
 
 plt.figure()
 plt.scatter( range(len(distances)), distances.mean(axis =1), s=9)
@@ -33,8 +33,8 @@ FN = 0
 highest_MCC = 0
 params = []
 
-for threshold in range(0, 2*int(max(distances.mean(axis=1)))):
-    outlier_index = np.where(distances.mean(axis = 1) > threshold/2)[0]
+for threshold in range(0, 10*int(max(distances.mean(axis=1)))):
+    outlier_index = np.where(distances.mean(axis = 1) > threshold/10)[0]
     TP = len(np.where(outlier_index >=  len(test_data)/2)[0])
     FP = len(outlier_index) - TP
     TN = len(test_data)/2 - FP
@@ -44,15 +44,19 @@ for threshold in range(0, 2*int(max(distances.mean(axis=1)))):
     # print('FP:', float(FP)) 
     # print('TN:', float(TN))
     # print('FN:', float(FN))
-    accuracy = utils.get_accuracy(TP = float(TP), FP = float(FP), FN = float(FN), TN = float(TN))
-    recall = utils.get_recall(TP = float(TP), FP = float(FP), FN = float(FN), TN = float(TN))
-    precision = utils.get_precision(TP = float(TP), FP = float(FP), FN = float(FN), TN = float(TN))
-    F1_score = utils.get_F1_score(TP = float(TP), FP = float(FP), FN = float(FN), TN = float(TN))
     MCC = utils.get_MCC(TP = float(TP), FP = float(FP), FN = float(FN), TN = float(TN))
+    if MCC > highest_MCC:
+        accuracy = utils.get_accuracy(TP = float(TP), FP = float(FP), FN = float(FN), TN = float(TN))
+        recall = utils.get_recall(TP = float(TP), FP = float(FP), FN = float(FN), TN = float(TN))
+        precision = utils.get_precision(TP = float(TP), FP = float(FP), FN = float(FN), TN = float(TN))
+        F1_score = utils.get_F1_score(TP = float(TP), FP = float(FP), FN = float(FN), TN = float(TN))
+        params = [threshold/10, accuracy, recall, precision, F1_score, MCC]
+        highest_MCC = MCC
+    else:
+        continue
+    
+    
     # print("accuracy: {}, recall: {}, precision: {}, F1_score: {}, MCC: {}".format(accuracy, recall, precision, F1_score, MCC))
     # print()
 
-    if MCC > highest_MCC:
-        params = [threshold/2, accuracy, recall, precision, F1_score, MCC]
-        highest_MCC = MCC
 print("threshold:{},accuracy: {}, recall: {}, precision: {}, F1_score: {}, MCC: {}".format(params[0],params[1], params[2], params[3], params[4], params[5]))
